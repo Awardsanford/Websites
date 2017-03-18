c4fac2b5b08cbc35b0097adc63116a580bda6ff0
Andrew Sanford <Awardsanford@gmail.com>
2017-03-18 15:00:42 -0400

    Added jquery file

---
+++ A_la_lola/jquery.js
@@ -0,0 +1,64 @@
+$(document).ready(function() {
+  
+ $('#pizza_button').on('click', function(e) {
+   
+ 
+    if($('#hide_pizza').is(":visible")){
+      $('#hide_pizza').hide();
+      $('#pizza_button').css({"background-color": "#C8C8C8"});
+    }
+   else{
+    
+    $('#hide_pizza').show();
+    $('#hide_ziti').hide();
+    $('#ziti_button').css({"background-color": "#C8C8C8"});
+   $('#hide_grinder').hide();
+   $('#grinder_button').css({"background-color": "#C8C8C8"});
+    $('#pizza_button').css({"background-color": "purple"})
+   }
+    
+  }
+)
+  
+  $('#ziti_button').on('click', function(e) {
+   
+    
+     if($('#hide_ziti').is(":visible")){
+      $('#hide_ziti').hide();
+      $('#ziti_button').css({"background-color": "#C8C8C8"});
+    }
+   else{
+   
+  
+      $('#hide_pizza').hide();
+    $('#pizza_button').css({"background-color": "#C8C8C8"});
+   $('#hide_grinder').hide();
+   $('#grinder_button').css({"background-color": "#C8C8C8"});
+    $('#hide_ziti').show();
+    $('#ziti_button').css({"background-color": "purple"})
+   }
+      })
+    
+
+ 
+   $('#grinder_button').on('click', function(e) {
+   
+     
+      if($('#hide_grinder').is(":visible")){
+      $('#hide_grinder').hide();
+      $('#grinder_button').css({"background-color": "#C8C8C8"});
+    }
+   else{
+   
+     
+
+      $('#hide_pizza').hide();
+    $('#pizza_button').css({"background-color": "#C8C8C8"});
+   $('#hide_ziti').hide();
+   $('#ziti_button').css({"background-color": "#C8C8C8"});
+    $('#hide_grinder').show();
+    $('#grinder_button').css({"background-color": "purple"})
+   }
+    
+  });
+   })
